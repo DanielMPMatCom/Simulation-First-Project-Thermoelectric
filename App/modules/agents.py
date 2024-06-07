57c70d7cd45a8c95f5ccbd3d5d092937b5fa81f5
Phann020126 <daniel020126@gmail.com>
2024-06-07 15:44:32 -0400

    agent_simulation

--- App/modules/agents.py
+++ App/modules/agents.py
@@ -1,19 +1,21 @@
 import random as rnd
 
 
-class Operator:
-    def __init__(self, options: "list[function]") -> None:  # funtions/0
-        self.options = options
+# class Operator:
+#     def __init__(self, options: "list[function]") -> None:  # funtions/0
+#         self.options = options
 
-    def decision(self, thermoElectrics, circuits):
-        pass
+#     def decision(self, thermoElectrics, circuits):
+#         pass
 
 
-class Random_Operator(Operator):
-    def __init__(self, options) -> None:
-        Operator.__init__(options)
+# class Random_Operator(Operator):
+#     def __init__(self, options) -> None:
+#         Operator.__init__(options)
 
-    def decision(self, thermoElectrics, circuits):
-        choice = rnd.randint(0, len(self.options) - 1)
-        return self.options[choice]()
+#     def decision(self, thermoElectrics, circuits):
+#         choice = rnd.randint(0, len(self.options) - 1)
+#         return self.options[choice]()\
+
+
         
