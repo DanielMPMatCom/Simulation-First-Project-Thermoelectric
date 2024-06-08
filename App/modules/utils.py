0e218c93763015a3234471bec8e478e1474021d0
DanielMPMatCom <daniel.machado.0206@gmail.com>
2024-06-08 00:28:28 -0400

    average fix

--- App/modules/utils.py
+++ App/modules/utils.py
@@ -1,6 +1,6 @@
 import thermoelectric
 
-def get_total_offer(thermoelectrics: list[thermoelectric.ThermoElectric]):
+def get_total_offer(thermoelectrics: "list[thermoelectric.ThermoElectric]"):
     offer = 0
     for thermoelectric in thermoelectrics:
         if thermoelectric.is_working():
