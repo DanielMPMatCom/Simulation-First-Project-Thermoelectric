91f438c7ad26a4981ba31fe6f78d59c565300966
DanielMPMatCom <daniel.machado.0206@gmail.com>
2024-06-09 00:58:06 -0400

    more graphics, fix deficit history

--- App/modules/circuit.py
+++ App/modules/circuit.py
@@ -13,67 +13,14 @@ class Circuit:
     def __init__(self, id: int, demand: float, days: int):
         self.id: int = id
         self.demand: float = demand
-        self.future_events = []
         self.deficit_history: list[float] = np.zeros(days)
         self.total_deficit: float = 0
-        self.days_disconnected: int = 0
         self.days: int = days
 
     def disconnect(self, percentage: float, current_day: int):
-        print(f"Disconnecting {self.demand * percentage} from circuit {self.id} on day {current_day}")
+        # print(f"Disconnecting {self.demand * percentage} from circuit {self.id} on day {current_day}")
         self.total_deficit += self.demand * percentage
-        print(f"Total deficit: {self.total_deficit}")
-        self.deficit_history[current_day] = self.demand * percentage
-        print(f"Today's deficit: {self.deficit_history[current_day]}")
-        self.days_disconnected += 1
-        print(f"Days disconnected: {self.days_disconnected}")
-
-    # def is_connected(self):
-    #     for thermoelectric in self.associated_thermoelectric:
-    #         if thermoelectric.is_working():
-    #             return True
-    #     return False
-
-    # def is_disconnected(self):
-    #     return not self.is_connected()
-
-    # def get_state(self):
-    #     return (
-    #         Circuit_State.CONNECTED
-    #         if self.is_connected()
-    #         else Circuit_State.DISCONNECTED
-    #     )
-
-    # def get_current_demand(self):
-    #     if self.is_disconnected():
-    #         return 0
-    #     return self.demand
-
-    def get_next_future_event_day(self) -> int:
-        pass
-
-    def pop_next_future_event(self) -> Event:
-        pass
-
-    def __generate_next_event(self, flip_flop: bool) -> Event:
-        pass
-
-    def planificate_events(
-        self, days: int, initial_state_flip_flop: bool = False
-    ) -> None:
-        pass
-
-    def repair_and_replanificate(self, days_to_replanificate) -> None:
-        pass
-
-    def get_history(self) -> "list[Event]":
-        pass
-
-    def get_distributions_info(self):
-        pass
-
-    def plot(self, from_day=0, to_day=None):  # WARNING IN PROCESS
-        pass
+        self.deficit_history[current_day] += self.demand * percentage
 
     def __str__(self):
         return f"Circuit {self.id} with demand {self.demand} and {self.days_disconnected} days disconnected"
